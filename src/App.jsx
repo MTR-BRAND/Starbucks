52b7f21ab1d46eb7979f1d4ab6e47ab842e318c5
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-11-03 11:05:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/nav'
+// import Home from './components/Home'
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      
+    </>
+  )
+}
+
+export default App
